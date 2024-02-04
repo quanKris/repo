00f92ec5ad9c1cec46bb58df3cd3ee372a1e8155
Quan Yuan <quan.yuan@mdpi.com>
2024-02-04 16:14:17 +0800

    commit

--- main.py
+++ main.py
@@ -16,13 +16,12 @@ app_secret = os.environ["APP_SECRET"]
 # 可把os.environ结果替换成字符串在本地调试
 user_ids = os.environ["USER_ID"].split(',')
 template_ids = os.environ["TEMPLATE_ID"].split(',')
-citys = os.environ["CITY"].split(',')
 
 
 
 # 当前城市、日期
-def get_city_date(city):
-    return city, today.date().strftime("%Y-%m-%d %H:%M:%S")
+def get_city_date():
+    return today.date().strftime("%Y-%m-%d %H:%M:%S")
 
 
 # 字体随机颜色
@@ -34,10 +33,10 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 for i in range(len(user_ids)):
-    cit, dat_time  = get_city_date(citys[i])
+    dat_time  = get_city_date()
     data = {
         "date": {"value": "当前时间：{}".format(dat_time), "color": get_random_color()},
-        "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
+        "city": {"value": "当前城市：大连 中国", "color": get_random_color()},
         "version":{"value": "1.0.0", "color": get_random_color()},
     }
     res = wm.send_template(user_ids[i], template_ids[i], data)
