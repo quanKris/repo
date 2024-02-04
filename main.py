a6456956303c96f1d01e95728d3ba4c87b1decd4
Quan Yuan <quan.yuan@mdpi.com>
2024-02-04 15:30:54 +0800

    commit

--- main.py
+++ main.py
@@ -19,49 +19,10 @@ template_ids = os.environ["TEMPLATE_ID"].split(',')
 citys = os.environ["CITY"].split(',')
 
 
-# 获取天气和温度
-def get_weather(city):
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-    res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp'])
-
 
 # 当前城市、日期
 def get_city_date(city):
-    return city, today.date().strftime("%Y-%m-%d")
-
-
-# 距离设置的日期过了多少天
-def get_count(start_date):
-    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-    return delta.days
-
-
-# 距离发工资还有多少天
-def get_solary(solary):
-    next = datetime.strptime(str(date.today().year) + "-" + str(date.today().month) + "-" + solary, "%Y-%m-%d")
-    if next < datetime.now():
-        if next.month == 12:
-            next = next.replace(year=next.year + 1)
-        next = next.replace(month=(next.month + 1) % 12)
-    return (next - today).days
-
-
-# 距离过生日还有多少天
-def get_birthday(birthday):
-    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-    if next < datetime.now():
-        next = next.replace(year=next.year + 1)
-    return (next - today).days
-
-
-# 每日一句
-def get_words():
-    words = requests.get("https://api.shadiao.pro/chp")
-    if words.status_code != 200:
-        return get_words()
-    return words.json()['data']['text']
+    return city, today.date().strftime("%Y-%m-%d %H:%M:%S")
 
 
 # 字体随机颜色
@@ -73,11 +34,12 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 
 for i in range(len(user_ids)):
-    cit, dat = get_city_date(citys[i])
+    cit, dat_time  = get_city_date(citys[i])
     data = {
-        "date": {"value": "今日日期：{}".format(dat), "color": get_random_color()},
+        "date": {"value": "当前时间：{}".format(dat_time), "color": get_random_color()},
         "city": {"value": "当前城市：{}".format(cit), "color": get_random_color()},
-        "words": {"value": get_words(), "color": get_random_color()}
+        "words": {"value": get_words(), "color": get_random_color()},
+        "version":{"value": "1.0.0", "color": get_random_color()},
     }
     res = wm.send_template(user_ids[i], template_ids[i], data)
     print(res)
